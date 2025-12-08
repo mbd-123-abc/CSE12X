3ff5d0634aa3d3059a10a793c211da4b1a7edcc0
mbd-123-abc <mahika13.3@gmail.com>
2025-12-07 18:52:18 -0800

    Create HelloBugs.java

---
+++ Homework/121/HelloBugs.java
@@ -0,0 +1,71 @@
+//Mahika Bagri
+//CSE 121
+//January 15 2025
+
+//This prints out bugs. Specifically, ladybug, ants, bee, butterfly, and spider. 
+public class HelloBugs {
+    public static void main(String[] args) {
+        //Hello Bugs! 
+        System.out.println("Hello Bugs! 🐞 🐝 🦗");
+
+        //First given bug: Ladybug. 
+        System.out.println();
+        System.out.println(" _|_|_|__");
+        System.out.println(" |       |\"-");
+        System.out.println(" |_______|\"-");
+        System.out.println("  | | |");
+
+        //Second given bug: ants. 
+        System.out.println();
+        System.out.println("Look! A trail of ants -> " +
+        ". . . . . . . . . . . . . . . . ." + 
+        " . . . . . . . . . . . . . . .");
+
+        //Third given bug: bee. 
+        System.out.println("Bzzzzzzzz...");
+        System.out.println();
+        System.out.println("    \\\\  \\\\");
+        System.out.println("    _\\\\__\\\\__/");
+        System.out.println("   _| | | | |/");
+        System.out.println("    |_|_|_|_|");
+
+        //Bye Bugs!
+        System.out.println();
+        System.out.println("Bye Bugs!");
+
+        //My bugs.
+        System.out.println();
+        System.out.println();
+        System.out.println("      My turn! 🦋 🕷️");
+        //Note to grader: Purposely center aligned. 
+
+        //First creative bug: Butterfly in its cocoon. 
+        System.out.println();
+        System.out.println("A butterfly in its cocoon:");
+        System.out.println("          .   .");
+        System.out.println("           \\ / ");
+        System.out.println("          (^-^)");
+        System.out.println("         (  |  )");
+        System.out.println("         (  |  )");
+        System.out.println("          ( | )");
+        System.out.println("            U");
+
+        //Second creative bug: Spider. 
+        System.out.println();
+        System.out.println("A spider hanging by a thread:");
+        System.out.println("             |"); 
+        System.out.println("             |");
+        System.out.println("             |   /---");
+        System.out.println("       ---\\ ' ' /"); 
+        System.out.println("      /----ooooo ----\\"); 
+        System.out.println("       /- ooooooo -\\");
+        System.out.println("      /    ooooo    \\"); 
+        System.out.println("        --/     \\--");
+        //Note to grader: Spider is purposely lopsided. 
+ 
+        //Caption
+        System.out.println();
+        System.out.print("Caption: ASCII art figures of a butterfly" + 
+        " and a spider by Mahika Bagri.");
+    }
+}
